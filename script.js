e21a269120a5dc8f9a451bab2b6e2f5af0150d6f
Ivan <jf561428@gmail.com>
2023-09-13 14:23:53 -0400

    Change dictionary for an API, added word length, and animations.

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 const MAX_INTENTOS = 6;
-const DICCIONARIO = ['BAILE', 'CALLE', 'DUROS', 'FALTA', 'GLOBO',
-'HUMOR', 'IDOLO', 'JABON', 'LARGO', 'MOLDE', 'NOCHE', 'QUITA', 
-'ROBLE', 'TOMAR', 'USADO', 'VALLE', 'ZUMBA'];
 
 let intentosRestantes = MAX_INTENTOS;
-let palabraSeleccionada = seleccionarPalabraAleatoria(DICCIONARIO);
+let palabraSeleccionada;
 const botonIntentar = document.getElementById("guess-button");
 const inputIntento = document.getElementById('guess-input');
 const container = document.getElementById('container')
 const botonReiniciar = document.getElementById("reset-button");
-
+const botonEmpezar = document.getElementById("start-button");
+const wordLengthSelector = document.getElementById("word-length");
+const selectorLength = document.getElementById(['word-lengthtxt'])
+const loading = document.getElementById('loading-message');
 
 
 botonIntentar.addEventListener('click', intentar);
 botonReiniciar.addEventListener('click', reiniciarJuego);
+botonEmpezar.addEventListener('click', iniciarJuego);
 
-function seleccionarPalabraAleatoria(diccionario) {
-    return diccionario[Math.floor(Math.random() * diccionario.length)];
+function seleccionarPalabraAleatoria() {
+    const wordLength = wordLengthSelector.value;
+    const API_URL = `https://random-word-api.herokuapp.com/word?length=${wordLength}&lang=es`;
+
+    return fetch(API_URL)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response[0].toUpperCase());
+            return response[0].toUpperCase();
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
+
+function iniciarJuego() {
+    wordLengthSelector.style.display = 'none';
+    botonEmpezar.style.display = 'none';
+    selectorLength.style.display = 'none'
+    loading.style.display = 'inline-block'
+
+    seleccionarPalabraAleatoria().then(palabra => {
+        inputIntento.setAttribute('maxlength', `${wordLengthSelector.value}`)
+        palabraSeleccionada = palabra;
+        inputIntento.style.display = 'block';
+        botonIntentar.style.display = 'inline-block';
+        loading.style.display = 'none';
+       
+    });
+}
+
+
 
 function intentar() {
     const intento = obtenerIntentoUsuario();
     const errorMessageDiv = document.getElementById('error-message');
     errorMessageDiv.textContent = ''; 
+    console.log(`intentando ${palabraSeleccionada}`);
     if (intento.length !== palabraSeleccionada.length) {
         errorMessageDiv.textContent = 'La palabra ingresada no tiene el tamaño correcto.';
         return;
@@ -33,16 +64,15 @@ function intentar() {
     grid.appendChild(row);
 
     if (intento === palabraSeleccionada) {
-        container.style.boxShadow = '0 14px 28px rgba(74, 189, 21, 0.25), 0 10px 10px rgba(17, 203, 27, 0.22)'
-        finalizarJuego('<h1>Ganaste :D<h1>');
+        finalizarJuego(0);
         return;
     }
 
     intentosRestantes--;
     if (intentosRestantes === 0) {
-        container.style.boxShadow = '0 14px 28px rgba(207, 14, 14, 0.25), 0 10px 10px rgba(221, 3, 3, 0.22)'
-        finalizarJuego('<h1>Perdiste D:<h1>');
+        finalizarJuego(1);
     }
+    inputIntento.value = ''
 }
 
 function obtenerIntentoUsuario() {
@@ -55,6 +85,7 @@ function crearRow(intento, palabra) {
 
     for (let i = 0; i < palabra.length; i++) {
         const letra = crearLetra(intento[i], palabra[i]);
+        letra.style.animationDelay = `${i * 0.5}s`;
         row.appendChild(letra);
     }
 
@@ -77,35 +108,39 @@ function crearLetra(letraIntento, letraPalabra) {
     return span;
 }
 
-function finalizarJuego(mensaje) {
-    const input = document.getElementById('guess-input');
-    const container = document.getElementById('guesses');
+function finalizarJuego(tipo) {
+    const guesses = document.getElementById('guesses');
 
-    input.disabled = true;
+    inputIntento.disabled = true;
     botonIntentar.disabled = true;
+    botonIntentar.style.display = 'none'
+    const tiempoTotalAnimacion = palabraSeleccionada.length * 0.5 * 1000; 
 
-    if (mensaje.includes('Ganaste')) {
-        container.innerHTML = '<h1 class="ganaste">Ganaste :D</h1>';
-    } else {
-        container.innerHTML = '<h1 class="perdiste">Perdiste D:</h1>';
-    }
-
-    botonReiniciar.style.display = 'inline-block';
+    setTimeout(function() {
+        botonIntentar.style.display = 'none'
+        if (tipo === 0){
+            container.style.boxShadow = '0 14px 28px rgba(74, 189, 21, 0.25), 0 10px 10px rgba(17, 203, 27, 0.22)'
+            guesses.innerHTML = '<h1 class="ganaste">Ganaste :D 🥳</h1>';
+        } else {
+            container.style.boxShadow = '0 14px 28px rgba(207, 14, 14, 0.25), 0 10px 10px rgba(221, 3, 3, 0.22)'
+            guesses.innerHTML = '<h1 class="perdiste">Perdiste D: 😵</h1>';
+        }
+        botonReiniciar.style.display = 'inline-block';
+    }, tiempoTotalAnimacion);
 }
 
 function reiniciarJuego() {
-    intentosRestantes = MAX_INTENTOS;
-    palabraSeleccionada = seleccionarPalabraAleatoria(DICCIONARIO);
-
-    const grid = document.getElementById("grid");
-    grid.innerHTML = '';
-    inputIntento.value = '';
-    
+    container.style.boxShadow = '0 10px 18px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)';
+    wordLengthSelector.style.display = 'inline-block';
+    botonEmpezar.style.display = 'inline-block';
+    selectorLength.style.display = 'inline-block'
+    inputIntento.style.display = 'none';
+    botonIntentar.style.display = 'none';
+    document.getElementById('grid').innerHTML = '';
+    document.getElementById('guesses').innerHTML = '';
+    botonReiniciar.style.display = 'none';
+    inputIntento.value= '';
     inputIntento.disabled = false;
     botonIntentar.disabled = false;
-    
-    const errorMessageDiv = document.getElementById('error-message');
-    errorMessageDiv.textContent = '';
-    const container = document.getElementById('guesses');
-    container.innerHTML = '';
+    intentosRestantes = MAX_INTENTOS;
 }
